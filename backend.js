c2ba8fe2a1b7b25c9358332fb4fbc1472ec340c1
gazianoyarhossain8-bit <gazianoyarhossain8@gmail.com>
2026-04-17 21:09:53 +0530

    backend frontend cors update

--- backend.js
+++ backend.js
@@ -13,10 +13,12 @@ app.use(cookieparser());
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
-     credentials: true,
-     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-     allowedHeaders: ['Content-Type', 'Authorization']
+    origin: ['http://localhost:5173',
+        'https://my-shop-silk-sigma.vercel.app'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
